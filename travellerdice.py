52ff6ca4dd20114958162ae94b06f5c7288018fc
David Caughell <davidcaughell111@gmail.com>
2022-10-01 15:02:25 -0400

    numbers_to_letters now converts a list to a string

--- travellerdice.py
+++ travellerdice.py
@@ -1,24 +1,20 @@
 #!/usr/bin/env python3
+'''Converts between letters and numbers for Traveller, when referring to
+numbers in letter form.'''
 
-'''This module handles a d66 roll, conversion of numbers to Traveller
-letternumbers and back, and generates random character stats, returning
-a list or a string, depending on your preference.
-It also rolls d6, 2d6, and does 1/3 and 1/2 checks.'''
-
-
-letternumbers = '0123456789ABCDEFGHJKLMNPQRSTUVWXYZ'
+ordered_numbers_string = '0123456789ABCDEFGHJKLMNPQRSTUVWXYZ'
 
 def letter_to_number(letter):
     '''Returns the number corresponding to a Traveller numberletter.
     This includes 0 through 9.'''
     assert len(letter) == 1, 'Single letter required.'
     letter = letter.upper()
-    out = letternumbers.find(letter)
+    out = ordered_numbers_string.find(letter)
     assert out != -1, 'Unknown character requested for translation.'
     return out
 
 def number_to_letter(num):
-    return letternumbers[num]
+    return ordered_numbers_string[num]
 
 def numbers_to_letters(numbers):
     '''Returns a string of all numbers converted to characters.'''
@@ -26,3 +22,19 @@ def numbers_to_letters(numbers):
     for n in map(number_to_letter, numbers):
         out += n
     return out
+
+def letters_to_numbers(numbers):
+    assert False, 'not done'
+    '''Returns a string of all numbers converted to characters.'''
+    out = ''
+    for n in map(number_to_letter, numbers):
+        out += n
+    return out
+
+assert letter_to_number('c') == 12
+assert letter_to_number('C') == 12
+assert letter_to_number('3') == 3
+assert letter_to_number('0') == 0
+assert number_to_letter(12) == 'C'
+assert number_to_letter(17) == 'H'
+assert numbers_to_letters([7,10,5,12,6,3]) == '7A5C63'
